6824c1ee242dce50fe25a7a8fa23c79712ea80c0
Asaf Izhak <asafizhak@outlook.com>
2025-05-22 18:47:24 +0300

    new

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,11 +1,2 @@
-output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
-}
-
-output "subnet_id" {
-  value = azurerm_subnet.subnet.id
-}
-
-output "application_gateway_id" {
-  value = azurerm_application_gateway.agw.id
-}
+# This file is intentionally left empty.
+# All outputs are defined in the main.tf file to avoid duplication.
